093e594c5a740f91b15946c1c33e1cfaec2101fa
Mikhail Loktev <ascrin96@gmail.com>
2019-04-27 10:38:31 +0300

    Add scrabing_aircrafts

---
+++ homework/scrabing_aircrafts/homework.py
@@ -0,0 +1,29 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+url = "https://www.nordstar.ru/about/park"
+
+r = requests.get(url)
+planes_dict = {}
+if r.status_code == 200:
+    content = r.text
+    bs = BeautifulSoup(content, "lxml")
+    models = bs.find_all("div", attrs={"class": "col-sm-6 col-xs-12 vs"})
+    models_image = bs.find_all("div", attrs={"class": "col-sm-6 col-xs-12 vsi"})
+
+
+    for model in models:
+        plane_dict = {}
+        plane_dict["number"] = model.find("img", attrs={"alt":"Количество в парке"}).parent.find("b").text
+        plane_dict["speed"] = model.find("img", attrs={"alt": "Крейсерская скорость"}).parent.find("b").text
+        plane_dict["chairs"] = model.find("img", attrs={"alt": "Количество мест"}).parent.find("b").text
+        plane_dict["distance"] = model.find("img", attrs={"alt": "Дальность полета"}).parent.find("b").text
+        planes_dict[model.find("h2").text] = plane_dict
+
+    for image in models_image:
+        name = image.previous_sibling.find("h2")
+        print(name)
+    print(planes_dict)
+
+
